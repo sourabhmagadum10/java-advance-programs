d374c2d2dbe699698c30e6e83093237b5f663414
unknown <sourabhmagadum11@gmail.com>
2025-09-11 13:44:44 +0530

    added one more object of student

--- advancejava/src/advancejava/StudentMain.java
+++ advancejava/src/advancejava/StudentMain.java
@@ -11,11 +11,13 @@ public class StudentMain {
 		Student s= new Student(4, "allen", "ENTC");
 		Student s1= new Student(2, "mask", "CSC");
 		Student s2= new Student(1, "jon", "BA");
+		Student s3= new Student(3,"sanju","Mcom");
 		
 		List <Student> l1= new ArrayList<Student>();
 		l1.add(s);
 		l1.add(s1);
 		l1.add(s2);
+		l1.add(s3);
 		
 //		Collections.sort(l1);
 //		System.out.println(l1);
