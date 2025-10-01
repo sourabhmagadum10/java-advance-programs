89d731eca63a1cc6508beb8a72271b794b324347
sourabhmagadum <sourabhmagadum11@gmail.com>
2025-10-01 23:38:21 +0530

    added string method ref

--- advancejava/src/methodreference/StringPrintMain.java
+++ advancejava/src/methodreference/StringPrintMain.java
@@ -4,7 +4,7 @@ public class StringPrintMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 		StringPrint s= String::toUpperCase;
 		String name=s.m1("santosh");
 		System.out.println(name);
